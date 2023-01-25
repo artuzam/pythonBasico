bb634a0c6833c3eb5652886e55e89a25249df8ba
artuzam <charliecazc@gmail.com>
2023-01-25 16:17:38 -0600

    Ejemplo sets

---
+++ Ejemplos/ejemplo_sets.py
@@ -0,0 +1,28 @@
+"""
+Ejemplos de uso de sets
+"""
+# Crear un nuevo set
+set_ejemplo = {1, 2, 3, 4}
+print(set_ejemplo)
+
+# Añadir elementos
+set_ejemplo.add(5)
+print(set_ejemplo)
+
+# Añadir otro set (o cualquier iterable)
+set_letras = {'a', 'b', 'c'}
+set_ejemplo.update(set_letras)
+print(set_ejemplo)
+
+# Eliminar un elemento
+set_ejemplo.remove('a')
+# set_ejemplo.remove('z')
+
+set_ejemplo.discard(1)
+set_ejemplo.discard('z')
+print(set_ejemplo)
+
+# Buscar un elemento
+if(3 in set_ejemplo):
+    print("3 está en el set")
+
