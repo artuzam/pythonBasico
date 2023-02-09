0122917cb30c8d96da29b008bb805908684239f7
artuzam <charliecazc@gmail.com>
2023-02-08 19:48:37 -0600

    Cambio de nombres de funciones y variables

--- SolucionTarea2/tarea2_sol.py
+++ SolucionTarea2/tarea2_sol.py
@@ -1,5 +1,5 @@
 # Factorial de un numero num
-def factorial(num):
+def calcular_factorial(num):
     if num < 0:
         return "El factorial de un negativo no existe"
     elif num == 0:
@@ -9,40 +9,47 @@ def factorial(num):
       for i in range(1, num+1):
           factorial = factorial * i
     return factorial
+
 # num = int(input("Ingrese un numero mayor o igual a 0: \n"))
-# print(factorial(num))
+# print(calcular_factorial(num))
 
 # triangulo de N niveles
-def triangulo(n):
+def dibujar_triangulo(n):
     if n <= 0:
-        return "El numero debe ser mayor a 0"
+        print("El numero debe ser mayor a 0")
+        return
     triangulo = ""
     for i in range(1, n+1):
         triangulo += str(i) + " "
         print(triangulo)
 # n = int(input("Ingrese un numero mayor 0: \n"))
-# triangulo(n)
+# dibujar_triangulo(n)
 
 # strings intercaladas
-def strings_intercaladas(str1, str2):
+def intercalar_strings(str1, str2):
     if len(str1) != len(str2):
         return "Las strings deben ser del mismo largo"
     res = ""
-    for i in range(0, len(str1) ):
+    for i in range(0, len(str1)):
         res += str1[i] + str2[i]
     return res
 # str1 = input("Ingrese la primera palabra \n")
 # str2 = input("Ingrese la segunda palabra \n")
-# print(strings_intercaladas(str1, str2))
+# print(intercalar_strings(str1, str2))
 
 # Lista al cubo
-def lista_al_cubo():
-    lista = [1, 2, 3, 4, 5]
+def elevar_al_cubo(lista):
     res = []
-    for i in lista:
-        res.append(i ** 3)
+    for item in lista:
+        res.append(item ** 3)
     return res
-# print(lista_al_cubo())
+
+# casos de prueba
+# print(elevar_al_cubo([1, 2, 3, 4, 5]))
+# print(elevar_al_cubo([1, 242, 578, 757354873]))
+# print(elevar_al_cubo([]))
+# print(elevar_al_cubo([3]))
+
 
 # Notas de clase
 sample_dict = {
@@ -58,21 +65,24 @@ sample_dict = {
     }
 }
 
-def notas_de_clase(dict):
+def calcular_notas(dict):
     nombre = dict["class"]["student"]["name"]
-    fisica = dict["class"]["student"]["marks"]["physics"]
-    historia = dict["class"]["student"]["marks"]["history"]
-    mate = dict["class"]["student"]["marks"]["math"]
-    promedio = (fisica + historia + mate)/3
+    nota_fisica = dict["class"]["student"]["marks"]["physics"]
+    nota_historia = dict["class"]["student"]["marks"]["history"]
+    nota_mate = dict["class"]["student"]["marks"]["math"]
+    promedio = (nota_fisica + nota_historia + nota_mate)/3
     return {"name": nombre, "avg": promedio}
-# print(notas_de_clase(sample_dict))
+# print(calcular_notas(sample_dict))
 
 # Eliminar los repetidos de una lista
 test_list = [1, 1, 2, 2, 3, 3, 3, 4, 5, 6, 6, 7, 7]
+test_list2 = ["pa", "pa", "pe", "pi"]
 def eliminar_reps(list):
     res = []
     for item in list:
         if item not in res:
             res.append(item)
     return res
-# print(eliminar_reps(test_list))
+print(eliminar_reps(test_list2))
+print(eliminar_reps([]))
+print(eliminar_reps([3,3,3,3,3,3,3]))
