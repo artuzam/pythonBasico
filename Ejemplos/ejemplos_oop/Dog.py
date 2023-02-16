eb6619d19f44a692baaebcb4b342c263e1e02881
artuzam <charliecazc@gmail.com>
2023-02-15 19:33:32 -0600

    Ejemplos OOP

---
+++ Ejemplos/ejemplos_oop/Dog.py
@@ -0,0 +1,55 @@
+class Dog:
+    # atributo de clase
+    species = "Canis familiaris"
+
+    def __init__(self, name, age):
+        # atributos de instancia
+        self.name = name
+        self.age = age
+
+    # métodos (comportamientos de mi clase)
+    def speak(self, sound):
+        print(f"{self.name} ladra diciendo: {sound}")
+
+    def getold(self):
+        self.age += 1
+
+
+mia = Dog("Mia", 3)  # instancia de la clase Dog
+pasi = Dog("Pasi", 5)  # otra instancia de la clase Dog
+
+# print(type(mia))
+# print(type(pasi))
+# print(mia == pasi)
+# print(mia.name)
+# print(mia.age)
+# print(mia.species)
+# print(pasi.species)
+
+
+class Dachshund(Dog):
+    # def speak(self, sound="Arf"):
+    #     print(f'{self.name} dice: {sound}')
+
+    # ejemplo de overriding (sobreescribir)
+    def speak(self, sound="arf"):
+        return super().speak(sound)
+
+    def getold(self):
+        self.age += 3
+
+    # ejemplo de extend
+    def sleep(self):
+        print("zzZZ")
+
+
+capitan = Dachshund("Capitán", 9)
+# print(capitan.age)
+# capitan.getold()
+# print(capitan.age)
+# capitan.speak("Como puedo hablar si soy un perro?")
+# mia.speak("Hola")
+# capitan.speak()
+
+# capitan.sleep()
+pasi.sleep()
---
+++ Ejemplos/ejemplos_oop/Employee.py
@@ -0,0 +1,65 @@
+import uuid
+
+
+class Employee:
+    # estos se llaman atributos de clase, se inicializan
+    # automáticamente
+    id = str(uuid.uuid4())
+
+    # todas las clases deben tener un método __init__ que
+    # inicializa sus atributos
+    def __init__(self, name, position, tenure):
+        # estos se llaman atributos de instacia
+        self.name = name
+        self.position = position
+        self.tenure = tenure
+
+    def get_employee_info(self):
+        tenure_in_years = self.tenure/12
+        return f"ID: {self.id} \nName: {self.name} \nPosition: {self.position} \nTenure: {tenure_in_years} years"
+
+    def speak(self, sound):
+        print(f"{self.name} dice: {sound}")
+
+
+pedro = Employee("Pedro", "Software Engineer", 6)
+maria = Employee("Maria", "QA Engineer", 24)
+# print(pedro.get_employee_info())
+# print(maria.get_employee_info())
+# pedro.speak("Hola")
+
+
+class Manager(Employee):
+    def __init__(self, name, position, tenure, juniors=[]):
+        super().__init__(name, position, tenure)
+        self.juniors = juniors
+
+    # def get_employee_info(self):
+    #     self.name = 'Mr. ' + self.name
+    #     return f"ID: {self.id} \nName: {self.name} \nPosition: {self.position} \nTenure: {self.tenure} months"
+
+    def get_employee_info(self):
+        self.name = 'Mr./Mrs. ' + self.name
+        return super().get_employee_info()
+
+    def list_juniors(self):
+        if (len(self.juniors) == 0):
+            print(f"{self.name} has no juniors")
+        else:
+            for employee in self.juniors:
+                print(employee.name)
+
+
+marco = Manager("Marco", "Project Manager", 50, [pedro, maria])
+luisa = Manager("Luisa", "QA Manager", 120)
+# print(luisa.get_employee_info())
+# print(type(marco))
+# print(type(pedro))
+print(isinstance(marco, Employee))
+print(isinstance(marco, Manager))
+# print(isinstance(luisa, Employee))
+print(isinstance(maria, Manager))
+# print(marco.get_employee_info())
+# marco.list_juniors()
+# luisa.list_juniors()
+# print(luisa.name)
