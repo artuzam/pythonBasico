9fde83f3453cfae7916ff26d6b0c81af923ac47c
artuzam <charliecazc@gmail.com>
2023-02-22 18:19:28 -0600

    Rename file and import

--- Ejemplos/ejemplos_proyecto1/main.py
+++ Ejemplos/ejemplos_proyecto1/main.py
@@ -1,10 +1,10 @@
-import Card as C
+import Classes
 import utils
 
 # crea una lista de 4 cartas que representa la mano de un jugador
 user_hand = []
 for i in range(4):
-    new_card = C.Card("bastos", 7, 7)
+    new_card = Classes.Card("bastos", 7, 7)
     user_hand.append(new_card)
 
 # invoca a la función de utils con la lista de Cards creada antes
